d76a02de38c775c0bb6649ce9dc92f260aefe4a1
Kurogi Kurumi <kurumi.kurogi@person-link.co.jp>
2022-01-06 17:36:36 +0900

    コンポーネントを追加

---
+++ src/components/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Router, Link } from 'svelte-routing';
+</script>
+
+<div>
+    <Router>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="create">Create</Link>  
+      </Router>
+</div>
